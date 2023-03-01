63f5d2660a0ac1104114121e74178f126e5dd6f9
Maqsood1532 <mchoudhary1532@gmail.com>
2023-03-01 17:30:27 +0100

    removed unwanted code

--- Factory/DepartmentFactory.cs
+++ Factory/DepartmentFactory.cs
@@ -10,23 +10,5 @@ namespace ParcelCompany.Factory
         {
             return new T();
         }
-
-        //public List<IDepartment> CreateDepartments()
-        //{
-        //    var departments = new List<IDepartment>();
-
-        //    // Create instances of the Mail, Regular, and Heavy classes based on the current business rules.
-        //    departments.Add(new Mail());
-        //    departments.Add(new Regular());
-        //    departments.Add(new Heavy());
-
-        //    // Add Insurance class if parcels with a value of over €1000,- need to be signed off.
-        //    //if (parcels.Value > 1000)
-        //    //{
-        //    //    departments.Add(new Insurance());
-        //    //}
-
-        //    return departments;
-        //}
     }
 }
